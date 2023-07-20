67b4394b37d91058fd3e021ad2438ed498eb6ed8
Alan Lansdowne <alan_lansdowne@yahoo.com>
2023-07-20 22:57:04 +0100

    Create convertJSONtoWebspaceSchema.php

---
+++ convertJSONtoWebspaceSchema.php
@@ -0,0 +1,23 @@
+<?php
+  
+function convertJSONtoWebspaceSchema ($webspaceJSON) {
+  
+  $webspaceJSON = str_replace('\"', '＂', $webspaceJSON);
+  $webspaceJSON = str_replace("'", "＇", $webspaceJSON);
+  $webspaceJSON = str_replace("`", "｀", $webspaceJSON);
+
+  $webspaceArray = explode('"', $webspaceJSON);
+  
+  for ($i = 1; $i < count($webspaceArray); $i = $i + 2) {
+
+    $webspaceArray[$i] = '«'.$webspaceArray[$i].'»';
+  }
+
+  $webspaceJSON = implode('', $webspaceArray);
+
+  $webspaceSchema = $webspaceJSON;
+
+  return $webspaceSchema;
+}
+
+?>
