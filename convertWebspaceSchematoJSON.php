762c820da94bcfb0a41cca728a30245eb4b033bc
Alan Lansdowne <alan_lansdowne@yahoo.com>
2023-07-21 17:51:17 +0100

    Update convertWebspaceSchematoJSON.php

--- convertWebspaceSchematoJSON.php
+++ convertWebspaceSchematoJSON.php
@@ -5,11 +5,11 @@ function convertWebspaceSchematoJSON ($webspaceSchema) {
   $webspaceSchema = str_replace('＂', '\"', $webspaceSchema);
   $webspaceSchema = str_replace("＇", "'", $webspaceSchema);
   $webspaceSchema = str_replace("｀", "`", $webspaceSchema);
+  $webspaceSchema = str_replace("‹", "<", $webspaceSchema);
+  $webspaceSchema = str_replace("›", ">", $webspaceSchema);
   $webspaceSchema = str_replace(['«', '»'], '"', $webspaceSchema);
 
   $webspaceJSON = $webspaceSchema;
-
-  echo $webspaceJSON;
 
   return $webspaceJSON;
 }
